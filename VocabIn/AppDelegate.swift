24e2dad9ef7930a7ebed63c282e7490cc8eb0d87
Bsheskee <bialobrzewski.business@gmail.com>
2023-01-01 21:12:00 +0100

    Test

--- VocabIn/AppDelegate.swift
+++ VocabIn/AppDelegate.swift
@@ -17,7 +17,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-        configureInitialViewController()
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+          // In project directory storyboard looks like Main.storyboard,
+          // you should use only part before ".storyboard" as its name,
+          // so in this example name is "Main".
+          let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+          
+          // controller identifier sets up in storyboard utilities
+          // panel (on the right), it is called 'Storyboard ID'
+          let viewController = storyboard.instantiateViewController(withIdentifier: "SearchVC") as! SearchVC
+
+          self.window?.rootViewController = viewController
+          self.window?.makeKeyAndVisible()
+        
         return true
     }
 
